d7725b6454ab9ba17cd64ed8c681de9ebd8d198a
4COLE <cole@cole-laptop>
2024-10-04 03:27:50 -0400

    added subtraction

--- app/main.py
+++ app/main.py
@@ -1,2 +1,6 @@
+#add function
 def addition (a,b):
 	return a + b
+
+def subtraction (a,b):
+	return a - b
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,6 +1,9 @@
 '''My Calc'''
-from app.main import addition
+from app.main import addition, subtraction
 
 def test_addition():
     '''Add function'''
     assert addition(1,1) == 2
+
+def test_subtraction():
+    assert subtraction(1,1) == 0
