2ce9b72edd5296646b982c814feb6af5902554a1
4COLE <cole@cole-laptop>
2024-10-04 05:09:00 -0400

    added operations

--- tests/test_operations.py
+++ tests/test_operations.py
@@ -1,6 +1,6 @@
 '''My Calc'''
 import pytest
-from app.main import addition, division, multiplication, subtraction
+from app.opperations import addition, division, multiplication, subtraction
 
 def test_addition():
     '''Add function'''
